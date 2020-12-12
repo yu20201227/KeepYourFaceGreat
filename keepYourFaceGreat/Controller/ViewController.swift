17a0d080bb1082e9754421e277b42d16ba237d6d
yu20201227 <yasada32020@gmail.com>
2020-12-12 10:41:29 +0900

    待ち受けアニメーションの追加

--- keepYourFaceGreat/Controller/ViewController.swift
+++ keepYourFaceGreat/Controller/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
+    
+    let animationView = AnimationView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        startAnimation()
 
+    }
+    func startAnimation(){
+        let path = Bundle.main.path(forResource: "8683-face-scanning", ofType: "json")
+        let animationView = AnimationView()
+        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        animationView.animationSpeed = 1.0
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
+        
     }
 
 
