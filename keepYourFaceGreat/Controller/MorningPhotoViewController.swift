bf68bf89039da1571002a4a4d6693c377d8623ea
yu20201227 <yasada32020@gmail.com>
2021-01-10 15:36:13 +0900

    morningPhotoButton実装

--- keepYourFaceGreat/Controller/MorningPhotoViewController.swift
+++ keepYourFaceGreat/Controller/MorningPhotoViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MorningPhotoViewController: UIViewController {
+class MorningPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     @IBOutlet weak var morningPhotoImageView: UIImageView!
     
@@ -19,6 +19,15 @@ class MorningPhotoViewController: UIViewController {
     }
     
     @IBAction func didTapAccessCameraButton(_ sender: UIButton) {
+        let UIImagePicker = UIImagePickerController()
+        UIImagePicker.sourceType = .camera
+        UIImagePicker.delegate = self
+        present(UIImagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let morningImage = info[.originalImage] as! UIImage
+        UIImageWriteToSavedPhotosAlbum(morningImage, nil, nil, nil)
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func didTapAccessLibraryButton(_ sender: UIButton) {
